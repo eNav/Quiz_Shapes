7a54bd08b4462330332bf915017557dd3ae2740c
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-11-05 10:27:12 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -3,9 +3,23 @@ package Goemetric_Computations;
  * Class for Square
  * variable side => length of square
  */
-public class Square extends Quadilateral {
+public class Square extends A_Quadrilateral {
 	
 	//Purpose: Implements a Square
 	public Square(double side) {
 		super(side, side);		}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double perimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
+

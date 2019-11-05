af4b5701df6ed31cf917c471fd59b56fc8917f4a
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-11-05 10:34:58 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -3,14 +3,14 @@ package Goemetric_Computations;
  * Class for Circle
  * variable r => radius
  */
-public class Circle implements IGeometry{
+public class Circle extends A_Shape{
 	
+	public Circle(double side) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	//Observers
 	protected double r;
-	
-	//Purpose: Implements a Circle
-	public Circle(double radius) {
-		r=radius;		}
 	
 				//Methods
 	//Purpose: To determine the area of the Circle
@@ -22,4 +22,29 @@ public class Circle implements IGeometry{
 	public double perimeter() {
 		//EQN Perimeter: PI X r X 2
 		return (Math.PI* r * 2);	}
+
+	
+	public double sides() {
+		// TODO Auto-generated method stub
+		return 1;
 	}
+
+	
+	public boolean isQuadrilateral() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean isTriangle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean isCircle() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	}
+

22f4ca9414af4d8f5df0c6a7d2d9a80e5021f830
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-11-05 10:29:49 -0500

    Update Quadilateral.java

--- Quadilateral.java
+++ Quadilateral.java
@@ -1,27 +1,6 @@
 package Goemetric_Computations;
-/**
- * Abstract Class for Quadilaterals
- * variable l => length
- * variable w => width
- */
-public abstract class Quadilateral implements IGeometry {
-	
-	//Observers
-	protected double l,w;
-		
-	//Purpose: Implements a Quadilateral
-	public Quadilateral(double length, double width) {
-		l=length; 
-		w=width;	}
-	
-				//Methods
-	//Purpose: To determine the area of the Quadilateral
-	public double area() {
-		//EQN Area: length X width
-		return (l * w);	}
-	
-	//Purpose: To determine the perimeter of the Quadilateral
-	public double perimeter() {
-		//EQN Perimeter: (length X 2) + (width X 2)
-		return ((l*2)+(w*2));	}	
+
+public interface Quadrilateral extends IGeometry{
+	public double getLength();
+
 }

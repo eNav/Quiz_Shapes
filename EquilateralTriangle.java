895096c610e87f49dfd5919da0038fc4b152126b
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-11-05 10:34:13 -0500

    Update EquilateralTriangle.java

--- EquilateralTriangle.java
+++ EquilateralTriangle.java
@@ -4,20 +4,12 @@ package Goemetric_Computations;
  * variable base => a
  * variable height => h
  */
-public class EquilateralTriangle extends Triangle{
+public class EquilateralTriangle extends A_Triangle{
 	
 	//Observers
-	protected double base,height;
 	
 	//Purpose: Implements an Equilateral Triangle
-	public EquilateralTriangle(double a, double h) {
-		super(a,a,a,h);
-		base=a;
-		height=h;	}
-		
-			//Methods
-	//Purpose: To determine the area of the Triangle
-	public double area() {
-		//EQN Area: Sqrt(3) X .25 X base X height
-		return (Math.sqrt(3)*.25*base*height );	}
-	}
+	public EquilateralTriangle(double s, double h) {
+		super(s,s,s,h);
+		}
+}
